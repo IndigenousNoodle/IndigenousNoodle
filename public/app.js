50dd25ab4524a9ad3dccf318cc2fb1eed7e755cd
Kevin Egami <kevin.egami@gmail.com>
2015-08-26 12:02:51 -0700

    (feat) add basic routing router ui

--- public/app.js
+++ public/app.js
@@ -1,2 +1,21 @@
-angular.module('app', [])
+angular.module('app', [
+  'ui.router'
+  ])
 
+.config(router)
+
+
+router.$inject = ['$urlRouterProvider', '$stateProvider', '$httpProvider'];
+
+
+
+function router($urlRouterProvider, $stateProvider, $httpProvider) {
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('homepage',{
+      url: '/',
+      templateUrl: './homepage/homepageTemplate.html',
+      controller: 'homepageController'
+    })
+}
