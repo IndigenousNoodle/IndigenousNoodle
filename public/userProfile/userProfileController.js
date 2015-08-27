a1b0927d6da067ab38c7a2c4c1b8331d9a96720e
Kevin Egami <kevin.egami@gmail.com>
2015-08-27 16:25:00 -0700

    (feat) add user profile page

---
+++ public/userProfile/userProfileController.js
@@ -0,0 +1,10 @@
+angular.module('app.userProfile',[])
+
+.controller('userProfileController', userProfileController);
+userProfileController.$inject = ['getProfilePrep'];
+
+function userProfileController (getProfilePrep) {
+  var vm = this;
+  vm.user = getProfilePrep.data;
+}
+
