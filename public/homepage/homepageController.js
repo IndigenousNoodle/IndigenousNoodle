d5154a2775c0e6f5fbd3f39a74e52aa9e76d0076
ingikim <gokim365@gmail.com>
2015-08-27 16:32:44 -0700

    (feat) Add homepage view, get events from the server and filter it with user input

--- public/homepage/homepageController.js
+++ public/homepage/homepageController.js
@@ -1,8 +1,27 @@
-angular.module("app")
-  .controller('homepageController', homePageController);
+angular.module('homepage', [])
+  .controller('homepageController', homepageController)
+  .directive('ngEnter', function() {
+    return function (scope, element, attrs) {
+      element.bind("keydown keypress", function(event) {
+        if(event.which === 13) {
+          scope.$apply(function() {
+            scope.$eval(attrs.ngEnter);
+          });
+          event.preventDefault();
+        }
+      });
+    };
+  });
 
-homePageController.$inject = [];
-
-function homePageController(navBarApp){
-  // use the navBarApp?
+function homepageController($scope, $http) { 
+  $scope.getData = function() {
+    return $http.get('/events').success(function(data) {
+      $scope.filteredData = data.filter(function(obj) {
+        if(obj.city.toLowerCase() === $scope.findCity.toLowerCase()) {
+          return true;
+        }
+        return false;
+      });
+    });
+  };
 }
