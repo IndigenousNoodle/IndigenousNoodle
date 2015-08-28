a5cf26d78431397e9bd71e06f72e3832a63a5c22
ingikim <gokim365@gmail.com>
2015-08-27 19:06:08 -0700

    (fix) Change homepage view

--- public/homepage/homepageController.js
+++ public/homepage/homepageController.js
@@ -1,6 +1,36 @@
-angular.module('homepage', [])
-  .controller('homepageController', homepageController)
-  .directive('ngEnter', function() {
+(function() {
+  angular.module('homepage', [])
+    .controller('homepageController', homepageController)
+    .directive('ngEnter', ngEnter);
+
+  // function homepageController($http) { 
+  //   var vm = this;
+  //   vm.getData = function() {
+  //     return $http.get('/events').success(function(data) {
+  //       vm.filteredData = data.filter(function(obj) {
+  //         if(obj.city.toLowerCase() === vm.findCity.toLowerCase()) {
+  //           return true;
+  //         }
+  //         return false;
+  //       });
+  //     });
+  //   };
+  // }
+
+  function homepageController($scope, $http) { 
+    $scope.getData = function() {
+      return $http.get('/events').success(function(data) {
+        $scope.filteredData = data.filter(function(obj) {
+          if(obj.city.toLowerCase() === $scope.findCity.toLowerCase()) {
+            return true;
+          }
+          return false;
+        });
+      });
+    };
+  }
+
+  function ngEnter() {
     return function (scope, element, attrs) {
       element.bind("keydown keypress", function(event) {
         if(event.which === 13) {
@@ -11,17 +41,5 @@ angular.module('homepage', [])
         }
       });
     };
-  });
-
-function homepageController($scope, $http) { 
-  $scope.getData = function() {
-    return $http.get('/events').success(function(data) {
-      $scope.filteredData = data.filter(function(obj) {
-        if(obj.city.toLowerCase() === $scope.findCity.toLowerCase()) {
-          return true;
-        }
-        return false;
-      });
-    });
-  };
-}
+  } 
+})();
