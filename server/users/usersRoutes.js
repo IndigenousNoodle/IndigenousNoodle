2f9ad6c7c8db614e0969cef387b59d7e5f2a1665
Kevin Egami <kevin.egami@gmail.com>
2015-08-27 21:57:36 -0700

    (feat) add basic host confirmation

--- server/users/usersRoutes.js
+++ server/users/usersRoutes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
   app.get('/user', usersController.getUser);
   app.get('/user/eventsManager', usersController.getEvents);
   app.get('/user/userProfile/:username', usersController.getProfile);
+  app.post('/user/confirmEvent/:username', usersController.confirmEvent);
 };
