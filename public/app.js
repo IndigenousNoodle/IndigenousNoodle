b949d79a7fdc5ce6c5bd61e5523760c5ce1fb12e
Kevin Egami <kevin.egami@gmail.com>
2015-08-27 21:18:33 -0700

    (feat) add joined events list, confirmed and waitlisted

--- public/app.js
+++ public/app.js
@@ -1,87 +1,88 @@
-angular.module('app', [
-  'app.navbar',
-  'app.createEvent',
-  'app.signupLogin',
-  'app.factories',
-  'ui.router',
-  'ui.bootstrap',
-  'homepage',
-  'eventList',
-  'app.dataservice',
-  'homepage',
-  'app.eventManager',
-  'app.userProfile'
-  ])
+(function(){
+  angular.module('app', [
+    'app.navbar',
+    'app.createEvent',
+    'app.signupLogin',
+    'app.factories',
+    'ui.router',
+    'ui.bootstrap',
+    'homepage',
+    'eventList',
+    'app.dataservice',
+    'homepage',
+    'app.eventManager',
+    'app.userProfile'
+    ])
 
-.config(router)
+  .config(router)
 
-router.$inject = ['$urlRouterProvider', '$stateProvider', '$httpProvider'];
+  router.$inject = ['$urlRouterProvider', '$stateProvider', '$httpProvider'];
 
-function router($urlRouterProvider, $stateProvider, $httpProvider) {
-  $urlRouterProvider.otherwise("/");
+  function router($urlRouterProvider, $stateProvider, $httpProvider) {
+    $urlRouterProvider.otherwise("/");
 
-  $stateProvider
-    .state('homepage',{
-      url: '/',
-      templateUrl: './homepage/homepageTemplate.html',
-      controller: 'homepageController',
-      controllerAs: 'home'
-    })
-    .state('createEvent', {
-      url: '/createEvent',
-      templateUrl: './createEventPage/createEventTemplate.html',
-      controller: 'createEventController',
-      controllerAs: 'event'
-    })
-    .state('eventList', {
-      url:'/eventList/:city',
-      templateUrl: './eventListPage/eventListTemplate.html',
-      controller: 'eventListController',
-      controllerAs: 'eventList',
-      resolve: {
-        getEventList: getEventList
+    $stateProvider
+      .state('homepage',{
+        url: '/',
+        templateUrl: './homepage/homepageTemplate.html',
+        controller: 'homepageController',
+        controllerAs: 'home'
+      })
+      .state('createEvent', {
+        url: '/createEvent',
+        templateUrl: './createEventPage/createEventTemplate.html',
+        controller: 'createEventController',
+        controllerAs: 'event'
+      })
+      .state('eventList', {
+        url:'/eventList/:city',
+        templateUrl: './eventListPage/eventListTemplate.html',
+        controller: 'eventListController',
+        controllerAs: 'eventList',
+        resolve: {
+          getEventList: getEventList
+        }
+      })
+      .state('signup',{
+        url: '/signup',
+        templateUrl: './signupLogin/signupTemplate.html',
+        controller: 'signupLoginController',
+        controllerAs: 'signupLogin'
+      })
+      .state('signin', {
+        url: '/signin',
+        templateUrl: './signupLogin/signinTemplate.html',
+        controller: 'signupLoginController',
+        controllerAs: 'signupLogin'
+      })
+      .state('eventManager', {
+        url: '/events/eventManager',
+        templateUrl: './eventManager/eventManagerTemplate.html',
+        controller: 'eventManagerController',
+        controllerAs: 'event',
+        resolve: {
+          getEventsPrep: getEventsService
+        }
+      })
+      .state('userProfile', {
+        url:'/user/userProfile/:username',
+        templateUrl: './userProfile/userProfileTemplate.html',
+        controller: 'userProfileController',
+        controllerAs: 'user',
+        resolve: {
+          getProfilePrep: getProfileService
+        }
+      })
+      function getEventList($http, $stateParams, dataservice) {
+        return dataservice.getEventList($stateParams.city);
       }
-    })
-    .state('signup',{
-      url: '/signup',
-      templateUrl: './signupLogin/signupTemplate.html',
-      controller: 'signupLoginController',
-      controllerAs: 'signupLogin'
-    })
-    .state('signin', {
-      url: '/signin',
-      templateUrl: './signupLogin/signinTemplate.html',
-      controller: 'signupLoginController',
-      controllerAs: 'signupLogin'
-    })
-    .state('eventManager', {
-      url: '/events/eventManager',
-      templateUrl: './eventManager/eventManagerTemplate.html',
-      controller: 'eventManagerController',
-      controllerAs: 'event',
-      resolve: {
-        getEventsPrep: getEventsService
+      function getEventsService ($http) {
+        return $http({method: 'GET', url: '/user/eventsManager'});
       }
-    })
-    .state('userProfile', {
-      url:'/user/userProfile/:username',
-      templateUrl: './userProfile/userProfileTemplate.html',
-      controller: 'userProfileController',
-      controllerAs: 'user',
-      resolve: {
-        getProfilePrep: getProfileService
+
+      function getProfileService ($http, $stateParams) {
+        return $http({method: 'GET', url: 'user/userProfile/' + $stateParams.username})
       }
-    })
-    function getEventList($http, $stateParams, dataservice) {
-      return dataservice.getEventList($stateParams.city);
-    }
-    function getEventsService ($http) {
-      return $http({method: 'GET', url: '/user/eventsManager'});
-    }
-
-    function getProfileService ($http, $stateParams) {
-      return $http({method: 'GET', url: 'user/userProfile/' + $stateParams.username})
-    }
-}
-
+  }
+})();
 
