8a67fe9f9bf3b86176e567aff6c7ac993db8e025
MichaelGofron <gofronm@yahoo.com>
2015-08-28 19:46:24 -0700

    (feat) work on join button in event detail page

--- public/eventDetailPage/eventDetailController.js
+++ public/eventDetailPage/eventDetailController.js
@@ -12,11 +12,24 @@
 
     /////////////////////////
 
+    // must have a get request for the event
+    // must have a get request for information about the host
+
     function join(){
       // push user onto list of events
       // update users joined events
       console.log("joining");
-      
+
+      var eventData = {id: 1, val: "hardcoded"};
+      var joiner = "Lisa";
+      var host = "Michael";
+
+      dataservice.joinEvent({eventData: eventData, joiner: joiner, host: host})
+      .then(function(data){
+        console.log("DATA === ", data);
+      }, function(err){
+        console.log("ERROR === ", err);
+      });
     }
   }
 
--- server/usersAndEvents/usersAndEventsController.js
+++ server/usersAndEvents/usersAndEventsController.js
@@ -27,9 +27,67 @@ var postEvents = function(req, res){
 
 var joinEvent = function(req, res){
   console.log("joining event, req.body === ", req.body);
+
+  // {eventData: event, joiner: joiner, host: host}
+  // must use eventID to reference the same event
+
+  // match the username
+  // match the event id of an event (object) in hostedEvents (array)
+  // push a user to the userApplied array
+
+  // must have an event with an id in eventData
+
+// { eventData: { id: 1, val: 'hardcoded' },
+//   joiner: 'Lisa',
+//   host: 'Michael' }
+
+
+// { 
+//   "_id" : ObjectId("55e0ecf9422d86f0a3b37b8f"),
+//    "username" : "Lisa", 
+//    "hostedEvents" : [ { "_id" : 2, "usersApplied" : [ ] } ], 
+//    "joinedEvents" : [ { "confirmed" : false } ] 
+// }
+
+Users.update({username:'Michael', "hostedEvents._id": "1"},
+  {$addToSet: {"hostedEvents.$.usersApplied": {"username":"SDasdfFSDFKJDSFKJSDFKJDSFKLJDSFLJSDF"}}});
+
 };
 
 module.exports = {
   postEvents: postEvents,
   joinEvent: joinEvent
 };
+
+  // Users.findOneAndUpdate(
+  //     {"username": req.body.host},
+  //     {$addToSet: {"usersApplied": req.body.eventData}},
+  //     {safe: true, new: true},
+  //     function(err, host){
+  //       if (err){
+  //         console.log("ERROR: ", err);
+  //         res.send(500,err);
+  //       }else{
+  //         // now we must find a joining user and push into the joinedEvents the eventData
+  //         // console.log("host === ", host);
+  //         // Users.findOneAndUpdate(
+  //         //   {"username": req.body.joiner},
+  //         //   {$addToSet: {"joinedEvents": req.body.eventData}},
+  //         //   {safe: true, new: true},
+  //         //   function(err, joiner){
+  //         //     if (err){
+  //         //       console.log("ERROR: ", err);
+  //         //       res.send(500, err);
+  //         //     }else{
+  //         //       console.log("Joiner === ", joiner);
+  //         //     }
+  //         //   }
+  //         // );
+  //       }
+  //     }
+  //   );
+
+  // find the host user
+  // then push into users applied the eventData
+  // find the joining user
+  // push into the joinedEvents the eventData
