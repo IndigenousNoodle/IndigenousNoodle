9a0530af2cb5a857497fd6a872b2dbcb4d8d6124
MichaelGofron <gofronm@yahoo.com>
2015-08-29 12:17:09 -0700

    (feat) passing data from eventList to eventDetail

--- public/eventDetailPage/eventDetailController.js
+++ public/eventDetailPage/eventDetailController.js
@@ -32,9 +32,7 @@
       });
     }
 
-    function getEvent(){
-      
-    }
+    
   }
 
 })();
--- public/eventListPage/eventListController.js
+++ public/eventListPage/eventListController.js
@@ -2,14 +2,27 @@
   angular.module('eventList', [])
     .controller('eventListController', eventListController)
 
-    function eventListController(getEventList, $state) {
+    eventListController.$inject = ['$state', 'getEventList'];
+
+    function eventListController($state, getEventList) {
       var vm = this;
       vm.getEventList = getEventList.data;
 
+      vm.toEventDetail = toEventDetail;
+
       vm.displayEvents = displayEvents;
+
+      ////////////////////////////////////
+
+      function toEventDetail(hostEvent){
+        // must pass the event and host data to the next screen
+        console.log("event === ", hostEvent);
+        $state.go('eventDetail');
+      }
 
       function displayEvents() {
         $state.go('eventList', {city: vm.findCity});
       }
+
     }
 })();
