b7a40c6d9640fa2398d035362abf47c27da52d1d
lisatsewang <lisatsewang@gmail.com>
2015-08-28 20:13:36 -0700

    (fix) Fix spacing

--- server/signupLogin/signupLoginController.js
+++ server/signupLogin/signupLoginController.js
@@ -7,34 +7,34 @@ var signup = function(req, res) {
 
 	// find user by username
   Users.findOne({username: req.body.username})
-  	.exec(function(err, data){
-  		if (err) { // if findOne err
-  			console.log("signup findOne error: ", err);
-  		} else { // if no findOne err
-  			if (!data) { // if no user found, save user
+    .exec(function(err, data){
+      if (err) { // if findOne err
+        console.log("signup findOne error: ", err);
+      } else { // if no findOne err
+        if (!data) { // if no user found, save user
           bcrypt.hash(req.body.password, null, null, function(err, hash) {
             if (err) {
               console.log("signup bcrypt err: ", err);
             } else {
-      				var newUser = new Users({username: req.body.username, password: hash});
-      				newUser.save(function(err, data) {
-      					if (err) { // if save user err
-      						console.log("signup .save error: ", err);
-      					}
+              var newUser = new Users({username: req.body.username, password: hash});
+              newUser.save(function(err, data) {
+                if (err) { // if save user err
+                  console.log("signup .save error: ", err);
+                }
                 console.log("newUser saved data: ", data);
                 var token = jwt.encode(data, 'localHostsSecretHostlocal');
                 console.log("token: ", token);
                 res.json({token: token});
-      				});
+              });
             }
           });
-  			} else { // if user already exist
-  				console.log("saveUser user already exist ", data);
+        } else { // if user already exist
+          console.log("saveUser user already exist ", data);
           res.send(null);
-  				// res.send("user already exist");
-  			}
-  		}
-  	});   
+          // res.send("user already exist");
+        }
+      }
+    });   
 };
 
 var signin = function(req, res) {
