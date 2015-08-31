09ca8d06bde7d04156e64cf28759b4aa3e4edeb0
ingikim <gokim365@gmail.com>
2015-08-31 11:45:28 -0700

    (feat) Add serach box to eventListPage using Polymer's element

--- public/eventListPage/eventListController.js
+++ public/eventListPage/eventListController.js
@@ -2,8 +2,14 @@
   angular.module('eventList', [])
     .controller('eventListController', eventListController)
 
-    function eventListController(getEventList) {
+    function eventListController(getEventList, $state) {
       var vm = this;
       vm.getEventList = getEventList.data;
+
+      vm.displayEvents = displayEvents;
+
+      function displayEvents() {
+        $state.go('eventList', {city: vm.findCity});
+      }
     }
 })();
