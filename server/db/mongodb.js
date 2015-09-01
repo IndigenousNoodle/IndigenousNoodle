fb831d214c71722a837d0710b5299bf8f788d1fe
Kevin Egami <kevin.egami@gmail.com>
2015-09-01 08:53:51 -0700

    (cleanup) remove console log tests

--- server/db/mongodb.js
+++ server/db/mongodb.js
@@ -3,7 +3,6 @@ var assert = require('assert');
 
 var url = 'mongodb://localhost/localhosts';
 MongoClient.connect(url, function(err, db) {
-  console.log('----------------9358723897')
   module.exports.instance = db;
   assert.equal(null, err);
   console.log("Connected correctly to server.");
