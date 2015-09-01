eac37456143b9a063085a9a7a51217cf383f053a
lisatsewang <lisatsewang@gmail.com>
2015-09-01 12:13:11 -0700

    (feat)Implement swapping sign in/sign out button

--- public/components/navBar/navBarDirective.js
+++ public/components/navBar/navBarDirective.js
@@ -1,18 +1,32 @@
 (function(){
   angular.module('app.navbar', ['ui.bootstrap'])
+    .controller('navController', navController)
     .directive('navigationBar', navBar);
 
-  navBar.$inject = ['Auth'];
+  navBar.$inject = [];
+  navController.$inject = ['$scope', '$state', 'Auth'];
+
+  function navController($scope, $state, Auth){
+  
+    $scope.checkAuth = function() {
+      if (Auth.isAuth()) {
+        $scope.signinoutMessage = 'Sign Out';
+        $scope.signinout = Auth.signout;
+      } else {
+        $scope.signinoutMessage = 'Sign In';
+        $scope.signinout = function() {
+          $state.go('signin');
+        };
+      }
+    }
+  }
 
   function navBar(Auth){
     return {
       restrict: "E",
       replace: true,
       transclude: true,
-      templateUrl: "./components/navBar/navBarTemplate.html",
-      link: function(navbar, elem, attrs) {
-        navbar.signout = Auth.signout;
-      }
+      templateUrl: "./components/navBar/navBarTemplate.html"
     };
   }
 })();
