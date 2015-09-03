5833764327a74f0ee5829486d0419db07f240cd0
Kevin Egami <kevin.egami@gmail.com>
2015-09-03 16:46:46 -0700

    (refactor) connect to postgresql and schema added

---
+++ server/db/newdb.js
@@ -0,0 +1,44 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('localhosts', 'root', '1234', {
+  dialect: 'postgres'
+});
+
+var Events = sequelize.define('events', {
+  title: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  city: Sequelize.STRING,
+  state: Sequelize.STRING,
+  zipcode: Sequelize.STRING,
+  cost: Sequelize.STRING,
+  time: Sequelize.STRING,
+  address: Sequelize.STRING,
+  photoUrl: Sequelize.TEXT
+
+});
+
+var Users = sequelize.define('users', {
+  username: {type: Sequelize.STRING, unique: true},
+  password: Sequelize.STRING,
+  photoUrl: Sequelize.TEXT
+});
+
+
+var Reviews = sequelize.define('reviews', {
+  review: Sequelize.STRING,
+  rating: Sequelize.INTEGER
+});
+
+var JoinersEvents = sequelize.define('joinersevents', {
+  confirmed: Sequelize.BOOLEAN
+})
+
+Users.belongsToMany(Events, {through: 'joinersevents'});
+Events.belongsToMany(Users, {through: 'joinersevents'});
+Users.hasMany(Reviews, {foreignKey: 'usersHostId'});
+Users.hasMany(Reviews, {foreignKey: 'usersJoinId'});
+Events.hasMany(Reviews);
+Users.hasMany(Events, {foreignKey: 'hostId'})
+
+
+module.exports = sequelize;
+
